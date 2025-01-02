cb6a0fc55034fbc4c62410271a1b2de9f2d68c62
teplostanski <igor@teplostanski.dev>
2025-01-02 12:58:09 +0300

    feat: add image modal with swiper and backdrop blur

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import data from './data.json'
 import { MasonryLayout } from './components/MasonryLayout'
 import { ImageCard } from './components/ImageCard'
 import { Header } from './components/Header'
+import ImageModal from './components/ImageModal'
 
 function App() {
   const { images } = data;
   const currentYear = new Date().getFullYear();
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
 
   return (
     <div className="p-5 md:p-10">
@@ -16,9 +19,16 @@ function App() {
             key={index}
             src={image.src}
             author={image.author}
+            onClick={() => setSelectedImageIndex(index)}
           />
         ))}
       </MasonryLayout>
+      <ImageModal
+        images={images}
+        initialSlide={selectedImageIndex || 0}
+        isOpen={selectedImageIndex !== null}
+        onClose={() => setSelectedImageIndex(null)}
+      />
     </div>
   )
 }
