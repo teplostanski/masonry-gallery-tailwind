c961564478437d466cf575dfe7e499acebbd801b
teplostanski <teplostanski@disroot.org>
2025-01-03 16:42:46 +0000

    chore: add deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/masonry-gallery-tailwind/',
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
