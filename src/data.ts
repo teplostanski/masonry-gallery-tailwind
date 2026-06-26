a804506b639a6158e5e692ccf016ec1805051c5b
teplostanski <contact@teplostanski.me>
2026-06-26 20:18:11 +0000

    refactor: migrate gallery data from JSON to TypeScript

---
+++ src/data.ts
@@ -0,0 +1,30 @@
+export type Image = {
+  id: number
+  author: string
+  src: string
+  width: number
+  height: number
+}
+
+export const images: Image[] = [
+  { id: 1, author: '@julian21', src: 'pictures/a8Sop90fcc0-unsplash.webp', width: 640, height: 427 },
+  { id: 2, author: '@julian21', src: 'pictures/22yEfat_3eQ-unsplash.webp', width: 640, height: 956 },
+  { id: 3, author: '@julian21', src: 'pictures/KQKB6KPBgAw-unsplash.webp', width: 640, height: 426 },
+  { id: 4, author: '@julian21', src: 'pictures/QkHuVpbmDeo-unsplash.webp', width: 640, height: 426 },
+  { id: 5, author: '@julian21', src: 'pictures/OY1irRHVUp0-unsplash.webp', width: 640, height: 962 },
+  { id: 6, author: '@julian21', src: 'pictures/VIrnnqGM8sE-unsplash.webp', width: 640, height: 956 },
+  { id: 7, author: '@julian21', src: 'pictures/dBvp-VEYfm8-unsplash.webp', width: 640, height: 426 },
+  { id: 8, author: '@julian21', src: 'pictures/w1asNzNnSKs-unsplash.webp', width: 640, height: 956 },
+  { id: 9, author: '@julian21', src: 'pictures/yOIUcfTNtbY-unsplash.webp', width: 640, height: 426 },
+  { id: 10, author: '@julian21', src: 'pictures/3tXJnQKJTVM-unsplash.webp', width: 640, height: 964 },
+  { id: 11, author: '@julian21', src: 'pictures/8ARo__JETc8-unsplash.webp', width: 640, height: 964 },
+  { id: 12, author: '@julian21', src: 'pictures/1zwcZmP21q8-unsplash.webp', width: 640, height: 427 },
+  { id: 13, author: '@julian21', src: 'pictures/VASM9eZ5UZc-unsplash.webp', width: 640, height: 956 },
+  { id: 14, author: '@julian21', src: 'pictures/1zLeimeRME4-unsplash.webp', width: 640, height: 428 },
+  { id: 15, author: '@julian21', src: 'pictures/J5qhOtH2Ldo-unsplash.webp', width: 640, height: 956 },
+  { id: 16, author: '@julian21', src: 'pictures/FMdWXa9W6XY-unsplash.webp', width: 640, height: 426 },
+  { id: 17, author: '@julian21', src: 'pictures/drov1I2YNVA-unsplash.webp', width: 640, height: 427 },
+  { id: 18, author: '@julian21', src: 'pictures/iqCf8uaQNCY-unsplash.webp', width: 640, height: 962 },
+  { id: 19, author: '@julian21', src: 'pictures/gESOae5Aqco-unsplash.webp', width: 640, height: 426 },
+  { id: 20, author: '@julian21', src: 'pictures/uZuTLONOmPk-unsplash.webp', width: 640, height: 427 },
+]
